e4a494588b4a8f2326e92b6b261250f36a76a5eb
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2013-12-03 17:47:55 +0100

    wip

--- main.js
+++ main.js
@@ -5,18 +5,74 @@ define(function (require, exports, module) {
 
     var CommandManager = brackets.getModule('command/CommandManager'),
         ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
+        NodeConnection = brackets.getModule('utils/NodeConnection'),
+        FileSystem = brackets.getModule('filesystem/FileSystem'),
         PreferencesManager = brackets.getModule('preferences/PreferencesManager'),
         AppInit = brackets.getModule('utils/AppInit'),
         COMMAND_ID = 'outofme.bracketsLivereload.enable',
+        nodeConnection,
+        lrDomain,
+        isRunning = false,
         preferences,
         $lrIcon;
 
-    function sayHello() {
-        console.log("Hello World");
+    function toggleTinyLR() {
+
+        if (!nodeConnection) {
+            connectNode();
+        } else {
+            toggle();
+        }
+
+        function toggle() {
+            console.log('Hello World');
+
+            lrDomain.toggleTinyLR(!isRunning, 35729).done(function () {
+                isRunning = !isRunning;
+
+                $lrIcon.removeClass('error active');
+                $lrIcon.addClass(isRunning ? 'active' : '');
+                $lrIcon.attr('title', 'Livereload: ' + (isRunning ? 'running' : 'disabled'));
+
+            }).fail(function (err) {
+
+                $lrIcon.addClass('error');
+                $lrIcon.removeClass('active');
+                $lrIcon.attr('title', 'Livereload has a problem toggling state: ' + err);
+
+                console.error('[brackets-livereload] failed to run livereload.toggleTinyLR', err);
+            });
+
+        }
+
+        function connectNode() {
+            nodeConnection = new NodeConnection();
+
+            var connectionPromise = nodeConnection.connect(true),
+                errorHandler = function (err) {
+                    nodeConnection = null;
+
+                    $lrIcon.addClass('error');
+                    $lrIcon.attr('title', 'Livereload is having a problem to connect node process: ' + err);
+
+                    console.log('[brackets-livereload] failed to load domain:', err);
+                };
+
+            connectionPromise.done(function () {
+                var path = ExtensionUtils.getModulePath(module, 'node/index'),
+                    loadPromise = nodeConnection.loadDomains([path], true);
+
+                loadPromise.done(function () {
+                    lrDomain = nodeConnection.domains.livereload;
+                    toggle();
+                }).fail(errorHandler);
+
+            }).fail(errorHandler);
+        }
     }
 
     // Register extension
-    CommandManager.register('Livereload', COMMAND_ID, sayHello);
+    CommandManager.register('Livereload', COMMAND_ID, toggleTinyLR);
 
     // Initialize PreferenceStorage.
     preferences = PreferencesManager.getPreferenceStorage(module, {
@@ -27,6 +83,12 @@ define(function (require, exports, module) {
         // Load stylesheet.
         ExtensionUtils.loadStyleSheet(module, 'livereload.css');
 
+//        FileSystem.on('change', function (e) {
+//            
+////            console.log('file changed', e.target);
+//        });
+
+        
         // Add icon to toolbar.
         $lrIcon = $('<a href="#" title="Livereload" id="brackets-livereload-icon"></a>');
         $lrIcon.click(function () {
--- node/index.js
+++ node/index.js
@@ -1,53 +1,64 @@
-/* jhint node:true */
+/* jshint node:true */
 
-var tinylr = require('tiny-lr');
+var tinylr = require('tiny-lr'),
+    server;
 
-//'use strict';
-//
-//var tinylr = require('tiny-lr');
-//
-//// Holds the servers out of scope in case watch is reloaded
-//var servers = Object.create(null);
-//
-//module.exports = function(grunt) {
-//
-//  var defaults = { port: 35729 };
-//
-//  function LR(options) {
-//    if (options === true) {
-//      options = defaults;
-//    } else if (typeof options === 'number') {
-//      options = {port: options};
-//    } else {
-//      options = grunt.util._.defaults(options, defaults);
-//    }
-//    if (servers[options.port]) {
-//      this.server = servers[options.port];
-//    } else {
-//      this.server = tinylr();
-//      this.server.server.removeAllListeners('error');
-//      this.server.server.on('error', function(err) {
-//        if (err.code === 'EADDRINUSE') {
-//          grunt.fatal('Port ' + options.port + ' is already in use by another process.');
-//        } else {
-//          grunt.fatal(err);
-//        }
-//        process.exit(1);
-//      });
-//      this.server.listen(options.port, function(err) {
-//        if (err) { return grunt.fatal(err); }
-//        grunt.log.verbose.writeln('Live reload server started on port: ' + options.port);
-//      });
-//      servers[options.port] = this.server;
-//    }
-//  }
-//
-//  LR.prototype.trigger = function(files) {
-//    grunt.log.verbose.writeln('Live reloading ' + grunt.log.wordlist(files) + '...');
-//    this.server.changed({body:{files:files}});
-//  };
-//
-//  return function(options) {
-//    return new LR(options);
-//  };
-//};
+function toggleTinyLR(start, port) {
+    console.log('toggling tiny-lr, start', start, 'port', port);
+
+    if (start) {
+        if (server) {
+            server.close();
+            server = null;
+        }
+
+        server = tinylr();
+        server.listen(port, function (err) {
+            if (err) {
+                return err;
+            }
+
+            console.log('Live reload server started on port: ' + port);
+
+            return;
+        });
+
+    } else {
+        server.close();
+        server = null;
+
+        console.log('Stopped livereload server at port: ' + port);
+
+        return;
+    }
+
+}
+
+function trigger(files) {
+    if (server) {
+        server.changed({
+            body: {
+                files: files
+            }
+        });
+    }
+}
+
+exports.init = function (DomainManager) {
+    if (!DomainManager.hasDomain('livereload')) {
+        DomainManager.registerDomain('livereload', {
+            major: 0,
+            minor: 1
+        });
+    }
+    DomainManager.registerCommand(
+        'livereload',
+        'toggleTinyLR',
+        toggleTinyLR,
+        false);
+    DomainManager.registerCommand(
+        'livereload',
+        'trigger',
+        trigger,
+        false);
+};
